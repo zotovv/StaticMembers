4e0bbc930100b5c04a2a5522448acd644b98baa7
Vladimir Zotov <zott14@outlook.com>
2018-05-29 18:39:32 +0300

    Add Delegation

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegation
+{
+    class Program
+    {
+        class Book
+        {
+            public void FileNext(string str)
+            {
+                Console.WriteLine("Поиск строки:" + str);
+            }
+
+        }
+
+        static class FindAndReplaceManager
+        {
+            static public void FileNext(string str)
+            {
+                Book a = new Book();
+                a.FileNext(str); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FindAndReplaceManager.FileNext("Hello");
+
+            Console.ReadKey();
+        }
+    }
+}
